a7882bcff047bc383d9a0241f4e81fffeb05a5fa
Keon-Woong Moon <cardiomoon@gmail.com>
2018-06-18 12:21:14 +0900

    0.2.2
    0.2.2

--- R/addLabelDf.R
+++ R/addLabelDf.R
@@ -1,12 +1,67 @@
-#'Add labels to the data.frame
+#' Add value labels to the data.frame
 #'@param data A data.frame
+#'@param mapping Set of aesthetic mappings created by aes or aes_.
 #'@importFrom sjmisc to_label
-#'@return A data.frame
 #'@export
-addLabelDf=function(data){
-        data.frame(lapply(data,function(x) to_label(x,add.non.labelled=TRUE)))
+addLabelDf=function(data,mapping=NULL){
+
+    if(!is.null(mapping)) {
+        (mapnames=names(mapping))
+        cols=c()
+        for(i in 1:length(mapnames)) {
+            temp=getMapping(mapping,mapnames[i])
+            # if(length(temp)>1) temp=temp[-1]
+            cols=c(cols,temp)
+        }
+        cols=unique(cols)
+        data[cols]=lapply(data[cols],function(x) to_label(x,add.non.labelled=TRUE))
+        # for(i in 1:length(cols)){
+        #
+        #         data[[cols[[i]]]]=to_label(data[[cols[i]]],add.non.labelled=TRUE)
+        # }
+    } else{
+        # cols=colnames(data)
+        # for(i in 1:length(cols)){
+        #         data[[cols[[i]]]]=to_label(data[[cols[i]]],add.non.labelled=TRUE)
+        # }
+        data[]=lapply(data,function(x) to_label(x,add.non.labelled=TRUE))
+    }
+    data
 }
 
+#' extract variable name from mapping, aes
+#' @param mapping aesthetic mapping
+#' @param varname variable name to extract
+#' @return variable name in character
+#' @importFrom stringr str_replace_all str_detect str_split fixed
+#' @importFrom utils packageVersion
+#' @export
+#' @examples
+#' require(ggplot2)
+#' mapping=aes(colour=sex)
+#' getMapping(mapping,"colour")
+#' getMapping(mapping,"x")
+getMapping=function(mapping,varname) {
+
+    # mapping=aes(colour=sex)
+    # varname="x"
+    if(is.null(mapping)) return(NULL)
+    result=paste(mapping[varname])
+    if(result=="NULL") result<-NULL
+    if(!is.null(result)){
+        if(packageVersion("ggplot2") > "2.2.1") {
+            result=stringr::str_replace_all(result,"~","")
+        }
+        result=stringr::str_replace_all(result,stringr::fixed("c("),"")
+        result=stringr::str_replace_all(result,stringr::fixed(")"),"")
+        result=stringr::str_replace_all(result," ","")
+        if(stringr::str_detect(result,",")) {
+            result=unlist(stringr::str_split(result,","))
+        }
+
+    }
+    result
+}
 
 #'Add column labels to the data.frame
 #'@param data A data.frame
